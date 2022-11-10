ec48b24998f3435ab0ae1c4a89331790c87a2a8f
tumbator <hgrejhuyft13fb6@mail.ru>
2022-11-10 11:53:28 +0300

    readme

--- start.sh
+++ start.sh
@@ -2,4 +2,10 @@
 
 mkdir tests
 mkdir modules
-mkdir utils
+
+curl -O https://github.com/tumbator/bash_scrpts/blob/master/requirements.txt
+curl -O https://github.com/tumbator/bash_scrpts/blob/master/.gitignore
+curl -O https://github.com/tumbator/bash_scrpts/blob/master/comm.sh
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
