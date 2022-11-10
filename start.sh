0156b4f3b389a39f996144e639cb6e1fc5ee4c32
tumbator <hgrejhuyft13fb6@mail.ru>
2022-11-10 12:20:26 +0300

    raw ссылки

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 mkdir tests
 mkdir modules
 
-curl -O https://github.com/tumbator/bash_scrpts/blob/master/requirements.txt
-curl -O https://github.com/tumbator/bash_scrpts/blob/master/.gitignore
-curl -O https://github.com/tumbator/bash_scrpts/blob/master/comm.sh
+curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/requirements.txt
+curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/.gitignore
+curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/comm.sh
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
