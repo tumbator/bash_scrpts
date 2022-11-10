e6131ca9f0199d4537fc77b2aea57555ad6c0f8d
tumbator <hgrejhuyft13fb6@mail.ru>
2022-11-10 14:45:17 +0300

    flake8 кфг файл + start

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ mkdir modules
 
 curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/requirements.txt
 curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/.gitignore
+curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/.flake8
 curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/comm.sh
 
 python -m pip install --upgrade pip
