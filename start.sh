a75ace85fb74e418533a61ae8d512b610783dec8
tumbator <hgrejhuyft13fb6@mail.ru>
2022-11-11 10:47:52 +0300

    project.md + start

--- start.sh
+++ start.sh
@@ -10,5 +10,7 @@ curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/mypy.ini
 curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/pytest.ini
 curl -O https://raw.githubusercontent.com/tumbator/bash_scrpts/master/comm.sh
 
+curl -O README.md https://raw.githubusercontent.com/tumbator/bash_scrpts/master/project.md
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
